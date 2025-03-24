ef50f322a808852794ed91c0cc314b518f005187
sqy <q.y.shi@stu.pku.edu.cn>
2025-03-24 18:50:12 +0800

    first commit

--- inference.py
+++ inference.py
@@ -1,98 +1,143 @@
-import os
+from src.modules.image_encoder import MSDiffusionImageEncoder
+from src.pipeline import MSDiffusionPipeline
+from src.utils import set_ms_adapter
+
 import torch
-from diffusers import StableDiffusionXLPipeline
 from PIL import Image
-from transformers import CLIPVisionModelWithProjection
-from transformers import CLIPImageProcessor
-
-from msdiffusion.models.projection import Resampler
-from msdiffusion.models.model import MSAdapter
-from msdiffusion.utils import get_phrase_idx, get_eot_idx
+from safetensors import safe_open
+from diffusers.models.unets.unet_2d_condition import UNet2DConditionModel
+from transformers import CLIPImageProcessor, CLIPTextConfig
 
 
-def get_phrases_idx(tokenizer, phrases, prompt):
-    res = []
-    phrase_cnt = {}
-    for phrase in phrases:
-        if phrase in phrase_cnt:
-            cur_cnt = phrase_cnt[phrase]
-            phrase_cnt[phrase] += 1
-        else:
-            cur_cnt = 0
-            phrase_cnt[phrase] = 1
-        res.append(get_phrase_idx(tokenizer, phrase, prompt, num=cur_cnt)[0])
-    return res
+def load_lora_weights(ckpt_path):
+    state_dict = {}
+    with safe_open(ckpt_path, framework="pt", device=0) as f:
+        for k in f.keys():
+            state_dict[k] = f.get_tensor(k)
+    return state_dict
 
 
-base_model_path = "/path/to/your/model"
-image_encoder_path = "/path/to/your/image_encoder"
-device = "cuda"
-result_path = "./res"
-log_id = "test"
-load_type = "checkpoint-xxxxxx"
-ms_ckpt = f"./output/{log_id}/{load_type}/ms_adapter.bin"
+pretrained_model_name_or_path = "/data00/sqy/checkpoints/stable-diffusion-xl-base-1.0"
+clip_model_name_or_path = "/data00/sqy/checkpoints/laion/CLIP-ViT-bigG-14-laion2B-39B-b160k"
+ms_ckpt = "/data00/sqy/checkpoints/MS-Diffusion/ms_adapter.bin"
+device = "cuda:2"
 
-image_processor = CLIPImageProcessor()
+lora_weights = "lora-weights/lora-trained-xl-hugging-kp-cross-0.6/pytorch_lora_weights.safetensors"
+class_names = ["cat", "plushie bear"]
+prompt = f"A cat painted on a stone."
+images_path = ["benchmark/cat_5/6.jpg",
+               "benchmark/plushie_bear/3.jpg"]
+boxes = [[[0.0, 0.203125, 0.546875, 0.859375], [0.546875, 0.265625, 0.9453125, 0.890625]]]
+phrases = [class_names]
 
-# load SDXL pipeline
-pipe = StableDiffusionXLPipeline.from_pretrained(
-    base_model_path,
-    torch_dtype=torch.float16,
-    add_watermarker=False,
+
+lora_weights = "lora-weights/lora-trained-xl-fighting-cross-0.6/pytorch_lora_weights.safetensors"
+class_names = ["dog", "cat", "plushie bear"]
+prompt = f"A cat is fighting with a dog and a plushie bear."
+images_path = ["benchmark/dog_0/1.jpg",
+               "benchmark/cat_1/1.jpg",
+               "benchmark/plushie_bear/3.jpg"]
+boxes = [[[0.0, 0.203125, 0.446875, 0.859375],
+          [0.446875, 0.214567, 0.696354, 0.87253],
+          [0.546875, 0.265625, 0.9453125, 0.890625]]]
+phrases = [class_names]
+
+
+# lora_weights = "lora-weights/lora-trained-xl-riding-cross/pytorch_lora_weights.safetensors"
+# class_names = ["elon musk"]
+# prompt = f"A elon musk is riding a bike, realism."
+# images_path = ["elon_musk.jpg"]
+# boxes = [[[0.25, 0.25, 0.75, 0.75]]]
+# phrases = [class_names]
+
+# lora_weights = "lora-weights/lora-trained-xl-cooking-cross/pytorch_lora_weights.safetensors"
+# class_names = ["person"]
+# prompt = f"A {class_names[0]} is cooking."
+# images_path = ["elon_musk.jpg"]
+# boxes = [[[0.25, 0.25, 0.75, 0.75]]]
+# phrases = [class_names]
+
+num_samples = 5
+scale = 0.6
+load_lora = False
+
+ms_state_dict = torch.load(ms_ckpt)
+image_encoder_state_dict = {}
+for key, value in ms_state_dict.items():
+    if key == "image_proj":
+        for k, v in value.items():
+            image_encoder_state_dict["resampler." + k] = v
+    elif key == "dummy_image_tokens":
+        image_encoder_state_dict[key] = value
+    else:
+        ms_adapter_state_dict = value
+
+unet = UNet2DConditionModel.from_pretrained(
+    pretrained_model_name_or_path, 
+    subfolder="unet"
+).to(device, torch.float16)
+adapter_modules = set_ms_adapter(unet, scale=scale)
+adapter_modules.load_state_dict(ms_adapter_state_dict)
+
+text_encoder_config = CLIPTextConfig.from_pretrained(
+    pretrained_model_name_or_path,
+    subfolder="text_encoder"
 )
-pipe.to(device)
 
-image_encoder_type = "clip"
-image_encoder = CLIPVisionModelWithProjection.from_pretrained(image_encoder_path).to(device, dtype=torch.float16)
-image_encoder_projection_dim = image_encoder.config.projection_dim
-num_tokens = 16
-image_proj_type="resampler"
-latent_init_mode="grounding"
-image_proj_model = Resampler(
+image_encoder = MSDiffusionImageEncoder(
+    clip_model_name_or_path,
     dim=1280,
     depth=4,
     dim_head=64,
     heads=20,
-    num_queries=num_tokens,
-    embedding_dim=image_encoder.config.hidden_size,
-    output_dim=pipe.unet.config.cross_attention_dim,
+    num_queries=16,
+    output_dim=unet.config.cross_attention_dim,
     ff_mult=4,
-    latent_init_mode=latent_init_mode,
-    phrase_embeddings_dim=pipe.text_encoder.config.projection_dim,
+    latent_init_mode="grounding",
+    phrase_embeddings_dim=text_encoder_config.projection_dim,
 ).to(device, dtype=torch.float16)
-ms_model = MSAdapter(pipe.unet, image_proj_model, ckpt_path=ms_ckpt, device=device, num_tokens=num_tokens)
-ms_model.to(device, dtype=torch.float16)
+image_encoder.load_state_dict(image_encoder_state_dict, strict=False)
 
-image0 = Image.open("./examples/example_dog.jpg")
-image1 = Image.open("./examples/example_cat.jpg")
-input_images = [image0]
-# input_images = [image0, image1]
+pipe = MSDiffusionPipeline.from_pretrained(
+    pretrained_model_name_or_path,
+    unet=unet,
+    image_encoder=image_encoder,
+).to(device=device, dtype=torch.float16)
+
+if load_lora:
+    pipe.load_lora_weights(lora_weights)
+
+input_images = [Image.open(image_path) for image_path in images_path]
 input_images = [x.convert("RGB").resize((512, 512)) for x in input_images]
 
-# generation configs
-num_samples = 5
-prompt = "best quality, high quality, a dog on the beach"
-# prompt = "best quality, high quality, a dog and cat on the beach"
-print(prompt)
-boxes = [[[0.25, 0.25, 0.75, 0.75]]]  # dog
-# boxes = [[[0., 0.25, 0.4, 0.75], [0.6, 0.25, 1., 0.75]]]  # dog+cat
-# boxes = [[[0., 0., 0., 0.], [0., 0., 0., 0.]]]  # used if you want no layout guidance
-phrases = [["dog"]]
-# phrases = [["dog", "cat"]]
-drop_grounding_tokens = [0]  # set to 1 if you want to drop the grounding tokens
+image_processor = CLIPImageProcessor()
+processed_images = [image_processor(images=input_images, return_tensors="pt").pixel_values]
+processed_images = torch.stack(processed_images, dim=0) # (1, n, 3, 224, 224)
 
-# used to get the attention map, return zero if the phrase is not in the prompt
-phrase_idxes = [get_phrases_idx(pipe.tokenizer, phrases[0], prompt)]
-eot_idxes = [[get_eot_idx(pipe.tokenizer, prompt)] * len(phrases[0])]
-print(phrase_idxes, eot_idxes)
+image_processor_896 = CLIPImageProcessor(
+    size=896,
+    crop_size=896
+)
+processed_images_896 = [image_processor_896(images=input_images, return_tensors="pt").pixel_values]
+processed_images_896 = torch.stack(processed_images_896, dim=0)  # (1, n, 3, 896, 896)
 
-images = ms_model.generate(pipe=pipe, pil_images=[input_images], num_samples=num_samples, num_inference_steps=30, seed=0,
-                           prompt=[prompt], scale=0.6, image_encoder=image_encoder, image_processor=image_processor, boxes=boxes,
-                           image_proj_type=image_proj_type, image_encoder_type=image_encoder_type, phrases=phrases, drop_grounding_tokens=drop_grounding_tokens,
-                           phrase_idxes=phrase_idxes, eot_idxes=eot_idxes, height=1024, width=1024)
+negative_prompt = "monochrome, lowres, bad anatomy, worst quality, low quality"
 
-save_name = "dog"
-save_path = os.path.join(result_path, log_id, load_type, save_name)
-os.makedirs(save_path, exist_ok=True)
-for i, image in enumerate(images):
-    image.save(os.path.join(save_path, f"{i}.jpg"))
+num = 0
+for i in range(32):
+    generator = torch.Generator(unet.device).manual_seed(i)
+    with torch.no_grad():
+        images = pipe(
+            prompt=prompt,
+            negative_prompt=negative_prompt,
+            concept_images=processed_images,
+            concept_images_896=processed_images_896,
+            num_inference_steps=30,
+            boxes=boxes,
+            phrases=phrases,
+            generator=generator,
+            num_images_per_prompt=num_samples
+        ).images
+        for i in range(len(images)):
+            num += 1
+            images[i].save(f"test{num}.jpg")
---
+++ train.sh
@@ -0,0 +1,20 @@
+export PYTHONPATH=.
+export MODEL_NAME="/data00/sqy/checkpoints/stable-diffusion-xl-base-1.0"
+export INSTANCE_DIR="./data/hug"
+export OUTPUT_DIR="./lora-weights/lora-trained-xl-hugging-0.001kp-0.001cal-0.6"
+
+accelerate launch scripts/train_keypoints.py \
+  --pretrained_model_name_or_path=$MODEL_NAME \
+  --instance_data_dir=$INSTANCE_DIR \
+  --output_dir=$OUTPUT_DIR \
+  --mixed_precision="fp16" \
+  --resolution=1024 \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=4 \
+  --learning_rate=1e-4 \
+  --report_to="wandb" \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --max_train_steps=500 \
+  --validation_epochs=1000 \
+  --seed="0"
